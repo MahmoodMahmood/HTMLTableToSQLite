ddef6b439c249926553644f62e77c1e588d4bacb
Mahmood Yahya <Mahmood Yahya>
2017-06-20 13:51:08 +0400

    FINALLY. TIS DONE!

--- HTMLTableGetter.py
+++ HTMLTableGetter.py
@@ -1,11 +1,22 @@
 import sqlite3
+import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 
-def init_db(cur, name):
+def createTable(cur, name):
     cur.execute("CREATE TABLE %s (DateVal TEXT PRIMARY KEY, Day TEXT,Emsak TEXT, "
                 "Fajr TEXT, Shurooq TEXT, Zuhr TEXT, Asr TEXT, Maghrib TEXT, Isha TEXT)" % name)
+
+def to_csv(db):
+    cursor = db.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = cursor.fetchall()
+    for table_name in tables:
+        table_name = table_name[0]
+        table = pd.read_sql_query("SELECT * from %s" % table_name, db)
+        table.to_csv(table_name + '.csv', index_label='index')
+
 
 def convertDayToEnglish(arabicDay):
     if arabicDay == 'السبت' :
@@ -33,7 +44,7 @@ def isCurrentHijriMonth(table): #current month has an extra column
         return False
 
 
-def runThroughMonth(cur, htmltext):
+def runThroughMonth(cur, htmltext, city):
     #grab this month's table from the current page
     soup = BeautifulSoup(htmltext, "lxml")
     table = soup.find('table', {'id': 'Contents_MonthlyPrayerTimes1_GridView1'})  # table we are looking for
@@ -62,29 +73,32 @@ def runThroughMonth(cur, htmltext):
             row = (date.text, convertDayToEnglish(arabicDay.text), emsak.text, fajr.text,
                    shurooq.text, zuhr.text, asr.text, maghrib.text, isha.text)
             # print (row)
-            cur.execute("INSERT INTO AbuDhabi VALUES (?,?,?,?,?,?,?,?,?)", row)
+            cur.execute("INSERT INTO %s VALUES (?,?,?,?,?,?,?,?,?)" % city, row)
     return
 
-db = sqlite3.connect(':memory:')
+db = sqlite3.connect('athanTimes.db')
 cur = db.cursor()
 url = "https://www.awqaf.gov.ae/MonthlyPrayerTimes.aspx?lang=EN"
 
 browser = webdriver.Firefox()
 browser.get(url)
 
-element = browser.find_element_by_id('Contents_MonthlyPrayerTimes1_ddlMonths')
+citiesBox = browser.find_element_by_name('ctl00$Contents$MonthlyPrayerTimes1$ddlCities')
+cities = []
+for element in citiesBox.find_elements_by_tag_name('option'): #grab all the cities
+    cities.append(element.text)#grab the city name
 
-init_db(cur, 'AbuDhabi')
-
-for i in range(12): #loop through the 12 months
-    select = Select(browser.find_element_by_name('ctl00$Contents$MonthlyPrayerTimes1$ddlMonths'))
-    month = select.select_by_index(i)
-    htmltext = browser.page_source
-    runThroughMonth(cur, htmltext)  # add all the data from that month to the database
+for city in cities:
+    selectCity = Select(browser.find_element_by_name('ctl00$Contents$MonthlyPrayerTimes1$ddlCities'))
+    selectCity.select_by_value(city)
+    city = city.replace(" ", "") #need to remove the whitespace to be a valid table name
+    city = city.replace("-", "") #this too :(
+    createTable(cur, city)
+    for i in range(12): #loop through the 12 months
+        selectMonth = Select(browser.find_element_by_name('ctl00$Contents$MonthlyPrayerTimes1$ddlMonths'))
+        selectMonth.select_by_index(i) # need to select by index due to page refreshing
+        htmltext = browser.page_source
+        runThroughMonth(cur, htmltext, city)  # add all the data from that month to the database
 
 db.commit()
-
-
-cur.execute("SELECT * FROM AbuDhabi")
-for element in cur.fetchall():
-    print (element)
+#to_csv(db) #uncomment this if you want to convert all the tables to csv format
